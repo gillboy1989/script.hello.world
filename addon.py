4249f2ffce1d66a0a5774bc4301bd8853df9f2f6
gillboy1989 <gillboy1989@users.noreply.github.com>
2019-12-23 15:56:47 -0800

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,11 +1,37 @@
+import logging
 import xbmcaddon
+from . import kodilogging
+from . import kodiutils
+from . import settings
+
+import sys
+
+from urllib import urlencode
+from urllib import quote
+from urlparse import parse_qsl
+
+from datetime import datetime
+
 import xbmcgui
+import xbmcplugin
+import xbmc
+
+import requests
+import itertools
+import operator
+
+import time
  
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-line1 = "Hello World!"
+line1 = "Alt Bala ji Addon!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
  
 xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+
+logger = logging.getLogger(ADD_ON.getAddonInfo('id'))
+kodilogging.config(logger)
+
+
